d96903fe6b34dee49eac0843f817d4dcd4e14569
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-21 22:51:49 +0700

    WIP - Messing with the concept of a demo page

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -12,6 +12,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
       new HtmlWebpackPlugin({filename: 'index.html', template: 'index.html'}),
       new HtmlWebpackPlugin({filename: 'cherry-brackets.html', template: 'cherry-brackets.html'}),
       new HtmlWebpackPlugin({filename: 'cherry-logo.html', template: 'cherry-logo.html'}),
-      new HtmlWebpackPlugin({filename: 'cherry-swarm.html', template: 'cherry-swarm.html'})
+      new HtmlWebpackPlugin({filename: 'cherry-swarm.html', template: 'cherry-swarm.html'}),
+      new HtmlWebpackPlugin({filename: 'demo.html', template: 'demo.html'})
     ]
   };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import './cherry-brackets';
 import './cherry-logo';
 import './cherry-swarm';
+import './cherry-button'
