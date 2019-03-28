a87e892efab215e4e01d58fbbf12a9670c50611b
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-28 17:59:13 +0700

    Updating demo.html and making things more consistent with my homepage

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,26 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const DEV_CONFIG = {
-  entry: './index.js',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({filename: 'index.html', template: 'index.html'}),
-    new HtmlWebpackPlugin({filename: 'cherry-brackets.html', template: 'cherry-brackets.html'}),
-    new HtmlWebpackPlugin({filename: 'cherry-logo.html', template: 'cherry-logo.html'}),
-    new HtmlWebpackPlugin({filename: 'cherry-swarm.html', template: 'cherry-swarm.html'}),
-    new HtmlWebpackPlugin({filename: 'demo.html', template: 'demo.html'})
-  ]
-};
-
 const PROD_CONFIG = {
   entry: './index.js',
   output: {
@@ -33,8 +13,32 @@ const PROD_CONFIG = {
     new HtmlWebpackPlugin({filename: 'cherry-logo.html', template: 'cherry-logo.html'}),
     new HtmlWebpackPlugin({filename: 'cherry-swarm.html', template: 'cherry-swarm.html'}),
     new HtmlWebpackPlugin({filename: 'demo.html', template: 'demo.html'})
-  ]
+  ],
+  module:{
+    rules:[{
+      test: /\.html$/,
+      use: [
+        'html-loader'
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader'
+      ]
+    }]
+  }
 };
+
+const DEV_CONFIG = Object.assign({}, PROD_CONFIG, {
+  mode: 'development',
+  devtool: 'inline-source-map'
+});
 
 module.exports = (env) => {
   return (env === 'dev' ? DEV_CONFIG : PROD_CONFIG);
