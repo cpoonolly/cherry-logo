86ba29fb31ee7c38a3e57be2fd3f492b3d1e6f73
Cherry Poonolly <rpoonolly@gmail.com>
2019-04-01 22:38:59 +0700

    still trying to get polyfills working for ie

--- homepage/index.js
+++ homepage/index.js
@@ -1,6 +1,4 @@
 import '../index';
-import '@webcomponents/webcomponentsjs/webcomponents-loader';
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
